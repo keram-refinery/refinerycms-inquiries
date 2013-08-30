01372046739872579908c2edf05115133df61d0d
Marek Labos <keraml@gmail.com>
2013-08-30 15:02:07 +0200

    update seed in honor of new Page.seed method

--- db/migrate/20101208082840_create_inquiries.rb
+++ db/migrate/20101208082840_create_inquiries.rb
@@ -1,14 +1,14 @@
 class CreateInquiries < ActiveRecord::Migration
   def up
     unless ::Refinery::Inquiries::Inquiry.table_exists?
-      create_table :refinery_inquiries_inquiries, :force => true do |t|
-        t.string   :name, :null => false
-        t.string   :email, :null => false
+      create_table :refinery_inquiries_inquiries, force: true do |t|
+        t.string   :name,     null: false
+        t.string   :email,    null: false
         t.string   :phone
-        t.text     :message, :null => false
-        t.boolean  :spam, :null => false, :default => false
-        t.boolean  :archived, :null => false, :default => false
-        t.timestamps
+        t.text     :message,  null: false
+        t.boolean  :spam,     null: false, default: false
+        t.boolean  :archived, null: false, default: false
+        t.timestamps null: false
       end
     end
   end
@@ -17,7 +17,7 @@ class CreateInquiries < ActiveRecord::Migration
      drop_table ::Refinery::Inquiries::Inquiry.table_name
 
      ::Refinery::Page.delete_all({
-       :plugin_page_id => ('refinery_inquiries' || 'refinery_inquiries_thank_you')
+       plugin_page_id: ('refinery_inquiries' || 'refinery_inquiries_thank_you')
      }) if defined?(::Refinery::Page)
   end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -6,65 +6,28 @@ plugin = Refinery::Plugins['refinery_inquiries']
   end
 end if defined?(::Refinery::User)
 
-if defined?(::Refinery::Page)
-  contact_us_page = plugin.page
-  # line Refinery::Page.find_by.. is required for proper running rake db:reset command
-  unless contact_us_page.present? && Refinery::Page.find_by(id: contact_us_page.id).present?
-    contact_us_page = ::Refinery::Page.create({
-      title: 'Contact',
-      show_in_menu: true,
-      plugin_page_id: plugin.name,
-      deletable: false
-    })
+pages = {
+  contact: {
+    title: 'Contact',
+    deletable: false,
+    plugin_page_id: plugin.name
+  },
+  thank_you: {
+    title: 'Thank You',
+    parent: :contact,
+    deletable: false,
+    show_in_menu: false,
+    plugin_page_id: "#{plugin.name}_thank_you"
+  },
+  privacy_policy: {
+    title: 'Privacy Policy',
+    deletable: true,
+    show_in_menu: false,
+    plugin_page_id: "#{plugin.name}_privacy_policy"
+  }
+}
 
-    contact_us_page.parts.create({
-      title: 'Body',
-      body: '<p>Get in touch with us. Just use the form below and we\'ll get back to you as soon as we can.</p>',
-      position: 0
-    })
-
-    contact_us_page.parts.create({
-      title: 'Side Body',
-      body: '',
-      position: 1
-    })
-  end
-
-  unless plugin.thank_you_page.present?
-    thank_you_page = contact_us_page.children.create({
-      title: 'Thank You',
-      show_in_menu: false,
-      plugin_page_id: "#{plugin.name}_thank_you",
-      deletable: false
-    })
-
-    thank_you_page.parts.create({
-      title: 'Body',
-      body: '<p>We\'ve received your inquiry and will get back to you with a response shortly.</p>' +
-                '<p><a href="/">Return to the home page</a></p>',
-      position: 0
-    })
-  end
-
-  unless plugin.privacy_policy_page.present?
-    privacy_policy_page = contact_us_page.children.create({
-      title: 'Privacy Policy',
-      plugin_page_id: "#{plugin.name}_privacy_policy",
-      deletable: true,
-      show_in_menu: false
-    })
-
-    privacy_policy_page.parts.create({
-      title: 'Body',
-      body: '<p>We respect your privacy. We do not market, rent or sell our email list to any outside parties.</p>' +
-            '<p>We need your e-mail address so that we can ensure that the people using our forms are bona fide. ' +
-            'It also allows us to send you e-mail newsletters and other communications, if you opt-in. ' +
-            'Your postal address is required in order to send you information and pricing, if you request it.</p>' +
-            '<p>Please call us at 123 456 7890 if you have any questions or concerns.</p>',
-      position: 0
-    })
-  end
-end
+Refinery::Pages.seed(plugin, pages)
 
 # todo move to locales
 inquiry_confirmation_body = "Thank you for your inquiry %name%,\n\n" +
