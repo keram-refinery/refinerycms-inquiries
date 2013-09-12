4b163bdf888e80c0a63547ef484ebe841c3dd74e
Marek Labos <keraml@gmail.com>
2013-09-12 20:57:57 +0200

    cleaned routes

--- config/routes.rb
+++ config/routes.rb
@@ -4,34 +4,34 @@ if plugin
     plugin.page.reload
 
     Refinery::Core::Engine.routes.draw do
-      namespace :inquiries, :path => '' do
+      namespace :inquiries, path: '' do
         if Refinery::Pages.marketable_urls
           Globalize.with_locales plugin.page.translated_locales do
-            get "#{plugin.page.nested_path}", :to => 'inquiries#new'
-            post "#{plugin.page.nested_path}", :to => 'inquiries#create'
+            get plugin.page.nested_path, to: 'inquiries#new'
+            post plugin.page.nested_path, to: 'inquiries#create'
           end
         else
-          resources :inquiries, :only => [:new, :create]
+          resources :inquiries, only: [:new, :create]
         end
       end
     end
   end
 
   Refinery::Core::Engine.routes.draw do
-    namespace :admin, :path => Refinery::Core.backend_route do
-      namespace :inquiries, :path => '' do
-        resources :inquiries, :only => [:index, :show, :destroy] do
+    namespace :admin, path: Refinery::Core.backend_route do
+      namespace :inquiries, path: '' do
+        resources :inquiries, only: [:index, :show, :destroy] do
           collection do
             get :spam
             get :archived
 
-            resources :settings, :only => [:edit, :update] do
-              match :confirmation_email, :via => [:get, :patch], :on => :collection
+            resources :settings, only: [:edit, :update] do
+              match :confirmation_email, via: [:get, :patch], on: :collection
             end
           end
 
-          get :toggle_spam, :on => :member
-          get :toggle_archive, :on => :member
+          get :toggle_spam, on: :member
+          get :toggle_archive, on: :member
         end
       end
     end
